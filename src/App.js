116d5d9579f7aec7970d4e19caee1015aeb73fd4
lurgi <wahoo9040@naver.com>
2023-09-21 16:01:52 +0900

    1. makeBridge refactor

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ const { Console } = require("@woowacourse/mission-utils");
 const BridgeGame = require("../src/BridgeGame");
 const BridgeMaker = require("./BridgeMaker");
 const { generate } = require("./BridgeRandomNumberGenerator");
+const { readBridgeSize } = require("./InputView");
 class App {
+  #bridge_size;
   async play() {
     /**
      * 1. 다리를 생성한다.
@@ -13,30 +15,24 @@ class App {
      *    BridgeGame.retry()함수 or BridgeGame().end()
      */
     Console.print("다리 건너기 게임을 시작합니다.");
-
-    //다리를 생성하는 코드
-    function readBridgeSize() {
-      return new Promise((resolve, reject) => {
-        Console.readLine("다리의 길이를 입력해주세요.", (input) => {
-          const INPUT_NUM = parseInt(input);
-          if (isNaN(INPUT_NUM)) reject(Error("숫자값을 입력하세요."));
-          resolve(INPUT_NUM);
-        });
-      });
-    }
-
-    let bridge_size;
+    //다리를 생성한다.
+    const BRIDGE = await this.makeBridgeToReadLenth();
+    //다리를 건넌다.
+  }
+  /**
+   * @returns {string[]}
+   * 1. 사이즈를 입력받고 다리를 생성합니다.
+   */
+  async makeBridgeToReadLenth() {
     while (true) {
       try {
-        bridge_size = await readBridgeSize();
+        this.#bridge_size = await readBridgeSize();
         break;
       } catch (error) {
         Console.print(error.message);
       }
     }
-
-    const BRIDGE = BridgeMaker.makeBridge(bridge_size, generate);
-    console.log(BRIDGE);
+    return BridgeMaker.makeBridge(this.#bridge_size, generate);
   }
 }
 
--- src/InputView.js
+++ src/InputView.js
@@ -1,17 +1,20 @@
 const { Console } = require("@woowacourse/mission-utils");
 // 유효성 검사 모듈
-const {
-  validQuestion,
-  validBridgeDistance,
-  validMoveInput,
-} = require("../src/validate/inputView");
 /**
  * 사용자로부터 입력을 받는 역할을 한다.
  * 사용자 값 입력을 위해 필요한 메서드를 추가할 수 있다.
  */
 const InputView = {
   /** 다리의 길이를 입력받는다.*/
-  readBridgeSize() {},
+  readBridgeSize() {
+    return new Promise((resolve, reject) => {
+      Console.readLine("다리의 길이를 입력해주세요.", (input) => {
+        const INPUT_NUM = parseInt(input);
+        if (isNaN(INPUT_NUM)) reject(Error("숫자값을 입력하세요."));
+        resolve(INPUT_NUM);
+      });
+    });
+  },
 
   /**
    * 사용자가 이동할 칸을 입력받는다.
