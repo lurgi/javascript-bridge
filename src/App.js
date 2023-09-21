d9f2ba9f2b2cd554b7094576333cf7759b1f8395
lurgi <wahoo9040@naver.com>
2023-09-21 13:28:22 +0900

    1.makeBridge

--- src/App.js
+++ src/App.js
@@ -1,5 +1,43 @@
+const { Console } = require("@woowacourse/mission-utils");
+const BridgeGame = require("../src/BridgeGame");
+const BridgeMaker = require("./BridgeMaker");
+const { generate } = require("./BridgeRandomNumberGenerator");
 class App {
-  play() {}
+  async play() {
+    /**
+     * 1. 다리를 생성한다.
+     *    BridgeMaker.makeBridge() 함수를 통해 클래스 생성을 통해서.
+     * 2. 다리를 건넌다
+     *    BridgeGame.move()함수
+     * 3. 재시작 혹은 게임 종료
+     *    BridgeGame.retry()함수 or BridgeGame().end()
+     */
+    Console.print("다리 건너기 게임을 시작합니다.");
+
+    //다리를 생성하는 코드
+    function readBridgeSize() {
+      return new Promise((resolve, reject) => {
+        Console.readLine("다리의 길이를 입력해주세요.", (input) => {
+          const INPUT_NUM = parseInt(input);
+          if (isNaN(INPUT_NUM)) reject(Error("숫자값을 입력하세요."));
+          resolve(INPUT_NUM);
+        });
+      });
+    }
+
+    let bridge_size;
+    while (true) {
+      try {
+        bridge_size = await readBridgeSize();
+        break;
+      } catch (error) {
+        Console.print(error.message);
+      }
+    }
+
+    const BRIDGE = BridgeMaker.makeBridge(bridge_size, generate);
+    console.log(BRIDGE);
+  }
 }
 
 module.exports = App;
--- src/BridgeMaker.js
+++ src/BridgeMaker.js
@@ -8,7 +8,15 @@ const BridgeMaker = {
    * @param {function(): number} generateRandomNumber 무작위 값을 생성해주는 함수
    * @return {string[]} 입력받은 길이에 해당하는 다리 모양. 위 칸이면 U, 아래 칸이면 D로 표현한다.
    */
-  makeBridge(size, generateRandomNumber) {},
+  makeBridge(size, generateRandomNumber) {
+    const makeCallback = () => {
+      const RANDOM_VALUE = generateRandomNumber();
+      if (RANDOM_VALUE) return "U";
+      if (!RANDOM_VALUE) return "D";
+    };
+    const BRIDGE = Array.from({ length: size }, makeCallback);
+    return BRIDGE;
+  },
 };
 
 module.exports = BridgeMaker;
