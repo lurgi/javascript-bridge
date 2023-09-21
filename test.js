67b814e7aa666a4a252fe3c659d917f51bde0b76
lurgi <wahoo9040@naver.com>
2023-09-21 12:02:23 +0900

    restart-01

--- test.js
+++ test.js
@@ -1,12 +1,3 @@
-const { readBridgeSize, waitReadLine } = require("./src/InputView");
-
-async function run() {
-  await readBridgeSize();
-}
-
-async function waitReadLine2() {
-  const Input = await waitReadLine("안녕하세요?");
-  console.log(Input);
-}
-
-run();
+const App = require("../src/App");
+const app = new App();
+app.play();
